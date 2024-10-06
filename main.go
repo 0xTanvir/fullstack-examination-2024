485e93dc11ae793620def46ea31d0c923f2783e6
0xTanvir <tanvir3080@gmail.com>
2024-10-06 20:47:09 +0600

    style(main): improve readability of swagger doc comments with better spacing

--- main.go
+++ main.go
@@ -6,13 +6,13 @@ import (
 	_ "github.com/zuu-development/fullstack-examination-2024/docs"
 )
 
-// @title			fullstack-examination-2024 API
-// @version		0.0.1
-// @description	This is a server for fullstack-examination-2024.
-// @license.name	Apache 2.0
-// @host			localhost:8080
-// @BasePath		/api/v1
-// @schemes		http
+//	@title			fullstack-examination-2024 API
+//	@version		0.0.1
+//	@description	This is a server for fullstack-examination-2024.
+//	@license.name	Apache 2.0
+//	@host			localhost:8080
+//	@BasePath		/api/v1
+//	@schemes		http
 func main() {
 	cmd.Execute()
 }
